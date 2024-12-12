679079bcebec2aad09bafac6ea8c23ad95dfb7e5
Juan Lagunas <dev.jlagunas@gmail.com>
2024-12-11 16:20:30 -0800

    refactor: TheNavbar - add nav tag for better semantic html and shorten css with mixin

--- src/components/reusableComponents/TheNavbar.vue
+++ src/components/reusableComponents/TheNavbar.vue
@@ -1,66 +1,58 @@
 <script setup></script>
 
 <template>
-  <ul class="navbar-container content-container">
-    <li class="navbar-item">
-      <router-link to="/" class="nav-link" active-class="nav-link-active"
-        >Home</router-link
-      >
-    </li>
-    <li class="navbar-item">
-      <router-link to="/about" class="nav-link" active-class="nav-link-active"
-        >About</router-link
-      >
-    </li>
-    <li class="navbar-item">
-      <router-link to="/contact" class="nav-link" active-class="nav-link-active"
-        >Contact</router-link
-      >
-    </li>
-    <li class="navbar-item">
-      <router-link to="/gallery" class="nav-link" active-class="nav-link-active"
-        >Gallery</router-link
-      >
-    </li>
-  </ul>
+  <nav class="content-container">
+    <ul class="the-navbar-container">
+      <li class="the-navbar-item">
+        <router-link
+          to="/"
+          class="the-navbar-link"
+          active-class="nav-link-active"
+          >Home</router-link
+        >
+      </li>
+      <li class="the-navbar-item">
+        <router-link
+          to="/about"
+          class="the-navbar-link"
+          active-class="nav-link-active"
+          >About</router-link
+        >
+      </li>
+      <li class="the-navbar-item">
+        <router-link
+          to="/contact"
+          class="the-navbar-link"
+          active-class="nav-link-active"
+          >Contact</router-link
+        >
+      </li>
+      <li class="the-navbar-item">
+        <router-link
+          to="/gallery"
+          class="the-navbar-link"
+          active-class="nav-link-active"
+          >Gallery</router-link
+        >
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
 //Mobile First Styles
-.navbar-container {
+.the-navbar-container {
   display: flex;
   justify-content: space-around;
-  margin-top: $spacing-reg;
-  text-transform: capitalize;
-  font-size: 1.5rem;
-  width: 100%;
+  font-size: $font-md;
+  width: $full-width;
+
+  .the-navbar-item {
+    @include highlight-style($primary-orange);
+  }
 
   .nav-link-active {
     border-bottom: 3px solid $primary-orange;
   }
-}
-
-// Custom Highligth Style
-li {
-  position: relative;
-  cursor: pointer;
-}
-
-li::before {
-  content: "";
-  position: absolute;
-  top: -2px;
-  left: 0;
-  right: 0;
-  height: 3px;
-  background-color: $primary-orange;
-  transform-origin: bottom right;
-  transform: scaleX(0);
-  transition: transform 0.5s ease;
-}
-
-li:hover::before {
-  transform-origin: bottom left;
-  transform: scaleX(1);
 }
 </style>
