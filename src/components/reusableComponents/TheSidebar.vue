c7172baa7421f45cf5afaba0d1b3b3477e4f3566
Juan Lagunas <dev.jlagunas@gmail.com>
2024-12-11 16:44:03 -0800

    refactor: TheSidebar - add aside html semantic tag and clean up css with mixins

--- src/components/reusableComponents/TheSidebar.vue
+++ src/components/reusableComponents/TheSidebar.vue
@@ -26,103 +26,84 @@ const isActive = (path) => route.path === path;
 </script>
 
 <template>
-  <ul
+  <aside
     :class="['the-sidenav-container', { 'is-open': isOpen }]"
     @click.self="emit('close')"
   >
-    <li
-      class="the-sidenav-item"
-      @click="routeTo('/')"
-      :class="{ 'side-link-active': isActive('/') }"
-    >
-      Home
-    </li>
-    <li
-      class="the-sidenav-item"
-      @click="routeTo('/about')"
-      :class="{ 'side-link-active': isActive('/about') }"
-    >
-      About
-    </li>
-    <li
-      class="the-sidenav-item"
-      @click="routeTo('/contact')"
-      :class="{ 'side-link-active': isActive('/contact') }"
-    >
-      Contact
-    </li>
-    <li
-      class="the-sidenav-item"
-      @click="routeTo('/gallery')"
-      :class="{ 'side-link-active': isActive('/gallery') }"
-    >
-      Gallery
-    </li>
-    <li>
-      <button
-        class="the-sidenav-item the-sidenav-item-btn"
-        @click="emit('close')"
+    <ul>
+      <li
+        class="the-sidenav-item"
+        @click="routeTo('/')"
+        :class="{ 'side-link-active': isActive('/') }"
       >
-        X
-      </button>
-    </li>
-  </ul>
+        Home
+      </li>
+      <li
+        class="the-sidenav-item"
+        @click="routeTo('/about')"
+        :class="{ 'side-link-active': isActive('/about') }"
+      >
+        About
+      </li>
+      <li
+        class="the-sidenav-item"
+        @click="routeTo('/contact')"
+        :class="{ 'side-link-active': isActive('/contact') }"
+      >
+        Contact
+      </li>
+      <li
+        class="the-sidenav-item"
+        @click="routeTo('/gallery')"
+        :class="{ 'side-link-active': isActive('/gallery') }"
+      >
+        Gallery
+      </li>
+      <li>
+        <button class="the-sidenav-item-btn" @click="emit('close')">X</button>
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <style lang="scss" scoped>
 .the-sidenav-container {
   @include flex-column-center;
   justify-content: center;
-  gap: 1.5rem;
   position: fixed;
   top: 0;
   right: 0;
-  width: 100%;
+  width: $full-width;
   height: 100vh;
   background-color: $primary-white;
   transition: transform 0.3s ease-in-out;
   z-index: 1000;
   transform: translateX(100%);
+  font-size: $font-md;
 
   &.is-open {
     transform: translateX(0);
   }
 
   .the-sidenav-item {
-    font-size: 1.5rem;
-    padding-top: 5px;
+    @include highlight-style($primary-orange);
+    margin-bottom: $spacing-reg;
+    width: max-content;
   }
 
   .the-sidenav-item-btn {
     font-weight: bold;
+    width: 100%;
+    font-size: 2rem;
+
+    &:hover {
+      transform: rotate(180deg);
+      transition: 1s all ease-in-out;
+    }
   }
 
   .side-link-active {
     border-bottom: 3px solid $primary-orange;
-  }
-}
-
-// Custom Highlight Style
-li {
-  position: relative;
-  cursor: pointer;
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: -2px;
-    left: 0;
-    right: 0;
-    height: 3px;
-    background-color: $primary-orange;
-    transform-origin: bottom right;
-    transform: scaleX(0);
-    transition: transform 0.5s ease;
-  }
-
-  &:hover::before {
-    transform-origin: bottom left;
-    transform: scaleX(1);
   }
 }
 </style>
