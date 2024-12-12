1a7a268d579bb8bb0aa38ff562960ec00df38a57
Juan Lagunas <dev.jlagunas@gmail.com>
2024-12-12 15:46:23 -0800

    refactor: FooterLandingSection - completely refactor UI for this component

--- src/components/footerSection/FooterLandingSection.vue
+++ src/components/footerSection/FooterLandingSection.vue
@@ -1,112 +1,131 @@
 <script setup>
-import TheNavbar from "@/components/reusableComponents/TheNavbar.vue";
-import harrisonImg from "@/assets/images/harrison-drawing.png";
 import pawPrint from "@/assets/svgs/paw-print.svg";
 </script>
 
 <template>
-  <footer class="footer-content-container">
-    <div class="footer-info-wrapper">
-      <h1 class="footer-info-thanks-text">Thanks Again!</h1>
-      <p class="footer-content-info-text">
-        <img
-          :src="pawPrint"
-          alt=""
-          class="footer-content-paw-img"
-        />619-555-5555
-      </p>
-      <p class="footer-content-info-text">
-        <img
-          :src="pawPrint"
-          alt=""
-          class="footer-content-paw-img"
-        />dog.days.harrisson@gmail.com
-      </p>
-      <TheNavbar class="the-navbar-tag" />
-    </div>
-    <div>
-      <p class="footer-content-question-text">
-        Have questions? Don't hesitate to reach out!
-      </p>
-      <img :src="harrisonImg" alt="" class="footer-content-drawing" />
-      <div class="footer-title-wrapper">
+  <footer class="the-footer-container content-container">
+    <button class="the-footer-scroll-btn">Back To Top</button>
+    <div class="the-footer-content-wrapper">
+      <ul class="the-footer-nav-wrapper">
+        <li class="the-navbar-item">
+          <router-link
+            to="/"
+            class="the-footer-link"
+            active-class="footer-nav-link-active"
+            >Home</router-link
+          >
+        </li>
+        <li class="the-navbar-item">
+          <router-link
+            to="/about"
+            class="the-footer-link"
+            active-class="footer-nav-link-active"
+            >About</router-link
+          >
+        </li>
+        <li class="the-navbar-item">
+          <router-link
+            to="/contact"
+            class="the-footer-link"
+            active-class="footer-nav-link-active"
+            >Contact</router-link
+          >
+        </li>
+        <li class="the-navbar-item">
+          <router-link
+            to="/gallery"
+            class="the-footer-link"
+            active-class="footer-nav-link-active"
+            >Gallery</router-link
+          >
+        </li>
+      </ul>
+      <ul class="the-footer-contact-wrapper">
+        <li>619-555-5555</li>
+        <li>dog.days.harisson@gmail.com</li>
+      </ul>
+      <div class="the-footer-title-wrapper">
         <h2>The Dog Days</h2>
         <p>with Harrison</p>
       </div>
     </div>
+    <button class="the-footer-privacy-btn">Privacy Policy</button>
+    <p class="the-footer-copyright">@ 2024 The Dog Days. All Rights Reserved</p>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-.footer-content-container {
-  margin-top: 5rem;
-  margin-bottom: 1rem;
+.the-footer-container {
+  @include flex-column;
+  background: $primary-blue;
+  padding: $spacing-reg;
+  border-radius: $radius-sm;
+  margin-bottom: $spacing-md;
+
+  .the-footer-scroll-btn {
+    width: max-content;
+    padding-block: 5px;
+    margin-right: auto;
+    margin-bottom: $spacing-md;
+  }
+}
+
+.the-footer-content-wrapper {
+  @include flex-column;
+  gap: $spacing-reg;
+
+  .the-footer-nav-wrapper {
+    @include flex-column;
+    gap: $spacing-reg;
+    font-size: $font-reg;
+
+    .the-footer-link {
+      @include highlight-style($primary-orange);
+    }
+
+    .footer-nav-link-active {
+      border-bottom: 3px solid $primary-orange;
+    }
+  }
+
+  .the-footer-contact-wrapper {
+    @include flex-column;
+    font-size: $font-reg;
+  }
+
+  .the-footer-title-wrapper {
+    font-size: $font-md;
+    margin-bottom: $spacing-md;
+  }
+}
+
+.the-footer-privacy-btn {
+  margin-bottom: $spacing-reg;
+}
+
+.the-footer-copyright {
   text-align: center;
 }
 
-.footer-content-paw-img {
-  height: 30px;
-}
-
-.footer-info-thanks-text {
-  font-size: 3rem;
-  margin-bottom: 1rem;
-}
-
-.the-navbar-tag {
-  margin-block: 2rem;
-}
-
-.footer-content-question-text {
-  background: $primary-blue;
-  color: $primary-white;
-  font-weight: bold;
-  padding: 15px;
-  border-top-left-radius: 50%;
-  border-bottom-right-radius: 10%;
-  width: 50%;
-  margin-inline: auto;
-}
-
-h2 {
-  font-size: 3rem;
-}
-
-.footer-content-info-text {
-  font-size: 1.3rem;
-  gap: 5px;
-  margin-bottom: 1rem;
-}
-
-.footer-title-wrapper {
-  margin-top: 1rem;
-}
-
-.footer-content-drawing {
-  max-height: 200px;
-  object-fit: contain;
-}
-
 @media (width >= 768px) {
-  .footer-content-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+  .the-footer-container {
+    .the-footer-scroll-btn {
+      margin-inline: auto;
+    }
+  }
+
+  .the-footer-content-wrapper {
+    flex-direction: row;
+    justify-content: space-around;
     align-items: center;
-    position: relative;
-    justify-items: center;
   }
+}
 
-  .the-navbar-tag {
-    gap: 20px;
-  }
-
-  .footer-content-info-text {
-    font-size: 1.5rem;
-  }
-
-  .footer-content-drawing {
-    max-height: 150px;
-    object-fit: contain;
+@media (width >= 1024px) {
+  .the-footer-content-wrapper {
+    .the-footer-nav-wrapper {
+      flex-direction: row;
+    }
   }
 }
 </style>
