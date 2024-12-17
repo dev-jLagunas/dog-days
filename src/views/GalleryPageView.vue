79e29e93757884f1f86cb0fa92f5838c1af9bb86
Juan Lagunas <dev.jlagunas@gmail.com>
2024-12-17 07:19:14 -0800

    refactor: GalleryPageView - completely refactor layout to simplify UI

--- src/views/GalleryPageView.vue
+++ src/views/GalleryPageView.vue
@@ -1,106 +1,66 @@
 <script setup>
 import dogImg1 from "@/assets/images/dog-walking-img1.jpg";
 import dogImg2 from "@/assets/images/dog-walking-img2.jpg";
-
 import dogImg3 from "@/assets/images/dog-walking-img3.jpg";
-
 import dogImg4 from "@/assets/images/dog-walking-img4.jpg";
-
 import dogImg5 from "@/assets/images/dog-walking-img5.jpg";
-
 import dogImg6 from "@/assets/images/dog-walking-img6.jpg";
 import pawPrint from "@/assets/svgs/paw-print.svg";
+import TheCallBtn from "@/components/reusableComponents/TheCallBtn.vue";
 </script>
 
 <template>
-  <div class="content-container gallery-content-container">
-    <div class="content-hero-img-wrapper">
-      <img
-        :src="dogImg1"
-        alt="Scenic dog walk through forest"
-        class="hero-img-forest"
-      />
-      <img :src="pawPrint" alt="" role="presentation" class="hero-img-paw-1" />
-      <img :src="pawPrint" alt="" role="presentation" class="hero-img-paw-2" />
-      <img
-        :src="dogImg2"
-        alt="A leashed dog sitting camly"
-        class="hero-img-dog"
-      />
-      <img
-        :src="dogImg3"
-        alt="A dog walk through the city"
-        class="hero-img-city"
-      />
-      <img :src="pawPrint" alt="" role="presentation" class="hero-img-paw-3" />
-    </div>
-    <div class="content-hero-img-wrapper">
-      <img
-        :src="dogImg4"
-        alt="Scenic dog walk through forest"
-        class="hero-img-forest"
-      />
-      <img :src="pawPrint" alt="" role="presentation" class="hero-img-paw-1" />
-      <img :src="pawPrint" alt="" role="presentation" class="hero-img-paw-2" />
-      <img
-        :src="dogImg5"
-        alt="A leashed dog sitting camly"
-        class="hero-img-dog"
-      />
-      <img
-        :src="dogImg6"
-        alt="A dog walk through the city"
-        class="hero-img-city"
-      />
-      <img :src="pawPrint" alt="" role="presentation" class="hero-img-paw-3" />
-    </div>
+  <div class="content-container gallery-imgs-container">
+    <img :src="dogImg1" alt="shiba inu on a leash" />
+    <img :src="dogImg2" alt="golden retriever on a leash" />
+    <img :src="dogImg3" alt="poodle looking at the camera" />
+    <img :src="pawPrint" alt="" class="paw-print-img" />
+    <img :src="dogImg4" alt="black and white dog on leash" />
+    <img :src="dogImg5" alt="puppy looking at the camera" />
+    <img :src="dogImg6" alt="walking dog on a hill" />
   </div>
+  <p class="gallery-footer-tagline">Happy and healthy doggies everywhere!</p>
+  <div class="call-btn-wrapper"><TheCallBtn /></div>
 </template>
 
 <style lang="scss" scoped>
-.content-hero-img-wrapper {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: auto;
+.gallery-imgs-container {
+  @include flex-column;
   gap: $spacing-reg;
-  margin-bottom: $spacing-reg;
-
-  img {
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-  }
+  max-width: 1200px;
 }
 
-.hero-img-forest {
-  grid-column: 1 / 5;
+img {
+  @include img-settings;
   object-fit: cover;
+  box-shadow: $custom-shadow;
+  max-height: 400px;
 }
 
-.hero-img-paw-1,
-.hero-img-paw-3 {
-  transform: rotate(12deg);
+.paw-print-img {
+  box-shadow: none;
 }
 
-.hero-img-paw-2 {
-  transform: rotate(-12deg);
+.gallery-footer-tagline {
+  text-align: center;
+  margin-top: $spacing-md;
+  font-size: $font-md;
 }
 
-.hero-img-dog {
-  grid-column: 2 / 6;
-  object-fit: cover;
+.call-btn-wrapper {
+  text-align: center;
 }
 
-.hero-img-city {
-  grid-column: 1 / 5;
-  object-fit: cover;
-  object-position: bottom;
-}
+@media (width >= 425px) {
+  .gallery-imgs-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: $spacing-reg;
 
-@media (width >= 768px) {
-  .content-hero-img-wrapper {
-    img {
-      max-height: 200px;
+    .paw-print-img {
+      grid-column: 1 / 4;
+      object-fit: contain;
+      height: 75px;
     }
   }
 }
